a4fe4f20a8bc88f8f89c86666c999627c072fc3d
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-12-09 12:01:02 +0000

    Application Module: workspace

---
+++ workspace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Main application Module"""
+
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import create_app, db
+from app.models import User, Post
+
+app = create_app()
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Post': Post}
