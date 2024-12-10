42ade723f92c8d9608d196cf83d08fa3168d3482
Susan Sewe <133601650+SUE2023@users.noreply.github.com>
2024-12-10 12:44:33 +0000

    Export posts general structure.

--- app/tasks.py
+++ app/tasks.py
@@ -21,3 +21,13 @@ def _set_task_progress(progress):
         if progress >= 100:
             task.complete = True
         db.session.commit()
+
+
+def export_posts(user_id):
+    try:
+        # read user posts from database
+        # send email with data to user
+    except Exception:
+        # handle unexpected errors
+    finally:
+        # handle clean up
